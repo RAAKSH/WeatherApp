e14a3f703730d3c140a135bbed5482dab095104d
raksha <rakshac29@gmail.com>
2023-02-01 23:33:07 +0530

    Weather and forecast app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import HomePageComponent from "./Components/HomePageComponent";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        backgroundImage: "url(/img/weatherImage1.jpg)",
+        backgroundRepeat: "none",
+        width: "2040 px",
+        height: "1000px",
+      }}
+    >
+      <h1 style={{ color: "whitesmoke", padding: "10px", margin: "20px" }}>
+        Weather Forecast{" "}
+      </h1>
+
+      <HomePageComponent />
     </div>
   );
 }
